2ea7a31d875114d1ccc65de47926e9ebd20071b4
MollikaComputer Provider <mollikacomputer3@gmail.com>
2023-07-23 00:01:45 +0600

    some paragraph added

--- src/Components/Auth/Register.js
+++ src/Components/Auth/Register.js
@@ -4,6 +4,7 @@ const Register = () => {
     return (
         <div>
             <h2 className='text-2xl font-bold'> Register page</h2>
+            <p>This is register page you can register here</p>
         </div>
     );
 };
